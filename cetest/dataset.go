5f3a28edfdc03393e2207d81d94cdbd371cc8397
Reminiscent <y870414423@gmail.com>
2020-12-10 15:15:45 +0800

    revert some change

--- cetest/dataset.go
+++ cetest/dataset.go
@@ -14,23 +14,8 @@ type Dataset interface {
 	// Name returns the name of the dataset
 	Name() string
 
-	// init is used to initialize the Dataset
-	init() error
-
-	// valArray returns a string array to store data according to the schema of the table
-	valArray() [][][]string
-
 	// GenCases ...
 	GenCases(n int, qt QueryType) (queries []string, err error)
-
-	randPointQuery(cols int) string
-	randPointColCond(tbIdx, colIdx int) string
-
-	randRangeQuery(cols int) string
-	randRangeColCond(tbIdx, colIdx int) string
-	randRangeQueryEQPrefix() string
-
-	randMCVLCVPointQuery(isMCV bool) string
 }
 
 type baseDataset struct {
@@ -41,13 +26,14 @@ type baseDataset struct {
 	numCols     []int
 	tbs         []string
 	cols        [][]string
+	used        [][]bool     // `used[i][j] = false` means we will not use the value of (table[i], column[j])
 	orderedVals [][][]string // [tbIdx][colIdx][]string{ordered values}
 	mcv         [][][]string // mcv[i][j] means the most common values in (table[i], column[j]) from the dataset
 	lcv         [][][]string // least common values
 	percent     int
 }
 
-func newBaseDataset(opt DatasetOpt, ins tidb.Instance, tbs []string, cols [][]string) (baseDataset, error) {
+func newBaseDataset(opt DatasetOpt, ins tidb.Instance, tbs []string, cols [][]string, used [][]bool) (baseDataset, error) {
 	numTbs := len(tbs)
 	numCols := make([]int, numTbs)
 	for idx, col := range cols {
@@ -60,18 +46,18 @@ func newBaseDataset(opt DatasetOpt, ins tidb.Instance, tbs []string, cols [][]st
 		numCols: numCols,
 		tbs:     tbs,
 		cols:    cols,
+		used:    used,
 		percent: 10, // most/least 10% common values
 	}, nil
-}
-
-func (ds *baseDataset) Name() string {
-	return ""
 }
 
 func (ds *baseDataset) init() error {
 	ds.orderedVals = ds.valArray()
 	for i, tb := range ds.tbs {
 		for j, col := range ds.cols[i] {
+			if !ds.used[i][j] {
+				continue
+			}
 			sql := fmt.Sprintf("SELECT %v FROM %v ORDER BY %v", col, tb, col)
 			begin := time.Now()
 			rows, err := ds.ins.Query(sql)
@@ -106,6 +92,9 @@ func (ds *baseDataset) init() error {
 		limit := total * ds.percent / 100
 
 		for j, col := range ds.cols[i] {
+			if !ds.used[i][j] {
+				continue
+			}
 			for _, order := range []string{"DESC", "ASC"} {
 				rows, err := ds.ins.Query(fmt.Sprintf("SELECT %v FROM %v GROUP BY %v ORDER BY COUNT(*) %v LIMIT %v", col, tb, col, order, limit))
 				if err != nil {
@@ -137,101 +126,13 @@ func (ds *baseDataset) valArray() [][][]string {
 	return xs
 }
 
-func (ds *baseDataset) GenCases(n int, qt QueryType) ([]string, error) {
-	if ds.orderedVals == nil {
-		if err := ds.init(); err != nil {
-			return nil, err
-		}
-	}
-
-	sqls := make([]string, 0, n)
-	switch qt {
-	case QTSingleColPointQuery:
-		for i := 0; i < n; i++ {
-			sqls = append(sqls, ds.randPointQuery(1))
-		}
-	case QTMultiColsPointQuery:
-		for i := 0; i < n; i++ {
-			sqls = append(sqls, ds.randPointQuery(2))
-		}
-	case QTSingleColRangeQuery:
-		for i := 0; i < n; i++ {
-			sqls = append(sqls, ds.randRangeQuery(1))
-		}
-	case QTMultiColsRangeQuery:
-		for i := 0; i < n; i++ {
-			sqls = append(sqls, ds.randRangeQuery(2))
-		}
-	case QTMultiColsRangeQueryEQPrefix:
-		for i := 0; i < n; i++ {
-			sqls = append(sqls, ds.randRangeQueryEQPrefix())
-		}
-	case QTMCVPointQuery:
-		for i := 0; i < n; i++ {
-			sqls = append(sqls, ds.randMCVLCVPointQuery(true))
-		}
-	case QTLCVPointQuery:
-		for i := 0; i < n; i++ {
-			sqls = append(sqls, ds.randMCVLCVPointQuery(false))
-		}
-	default:
-		return nil, errors.Errorf("unsupported query-type=%v", qt.String())
-	}
-	return sqls, nil
-}
-
-// todo: Support query conditions with more than two columns
-func (ds *baseDataset) randPointQuery(cols int) string {
-	tbIdx := rand.Intn(ds.numTbs)
-	cond := ""
-	if cols == 1 {
-		cond = ds.randPointColCond(tbIdx, rand.Intn(ds.numCols[tbIdx]))
-	} else {
-		// TODO: check the ds.numTbs and generate random column index to construct the condition
-		cond = fmt.Sprintf("%v AND %v", ds.randPointColCond(tbIdx, 0), ds.randPointColCond(tbIdx, 1))
-	}
-	return fmt.Sprintf("SELECT * FROM %v WHERE %v", ds.tbs[tbIdx], cond)
-}
-
 func (ds *baseDataset) randPointColCond(tbIdx, colIdx int) string {
 	val := ds.orderedVals[tbIdx][colIdx][rand.Intn(len(ds.orderedVals[tbIdx][colIdx]))]
 	return fmt.Sprintf("%v = %v", ds.cols[tbIdx][colIdx], val)
-}
-
-// todo: Support query conditions with more than two columns
-func (ds *baseDataset) randRangeQuery(cols int) string {
-	tbIdx := rand.Intn(ds.numTbs)
-	cond := ""
-	if cols == 1 {
-		cond = ds.randRangeColCond(tbIdx, rand.Intn(ds.numCols[tbIdx]))
-	} else {
-		// TODO: check the ds.numTbs and generate random column index to construct the condition
-		cond = fmt.Sprintf("%v AND %v", ds.randRangeColCond(tbIdx, 0), ds.randRangeColCond(tbIdx, 1))
-	}
-	return fmt.Sprintf("SELECT * FROM %v WHERE %v", ds.tbs[tbIdx], cond)
 }
 
 func (ds *baseDataset) randRangeColCond(tbIdx, colIdx int) string {
 	val1Idx := rand.Intn(len(ds.orderedVals[tbIdx][colIdx]))
 	val2Idx := rand.Intn(len(ds.orderedVals[tbIdx][colIdx])-val1Idx) + val1Idx
 	return fmt.Sprintf("%v>=%v AND %v<=%v", ds.cols[tbIdx][colIdx], ds.orderedVals[tbIdx][colIdx][val1Idx], ds.cols[tbIdx][colIdx], ds.orderedVals[tbIdx][colIdx][val2Idx])
-}
-
-func (ds *baseDataset) randRangeQueryEQPrefix() string {
-	tbIdx := rand.Intn(ds.numTbs)
-	// TODO: check the ds.numTbs and generate random column index to construct the condition
-	cond := fmt.Sprintf("%v AND %v", ds.randPointColCond(tbIdx, 0), ds.randRangeColCond(tbIdx, 1))
-	return fmt.Sprintf("SELECT * FROM %v WHERE %v", ds.tbs[tbIdx], cond)
-}
-
-func (ds *baseDataset) randMCVLCVPointQuery(isMCV bool) string {
-	tbIdx := rand.Intn(ds.numTbs)
-	colIdx := rand.Intn(ds.numCols[tbIdx])
-	val := ""
-	if isMCV {
-		val = ds.mcv[tbIdx][colIdx][rand.Intn(len(ds.mcv[tbIdx][colIdx]))]
-	} else {
-		val = ds.lcv[tbIdx][colIdx][rand.Intn(len(ds.lcv[tbIdx][colIdx]))]
-	}
-	return fmt.Sprintf("SELECT * FROM %v WHERE %v=%v", ds.tbs[tbIdx], ds.cols[tbIdx][colIdx], val)
 }
--- cetest/dataset_zipfx.go
+++ cetest/dataset_zipfx.go
@@ -1,6 +1,10 @@
 package cetest
 
 import (
+	"fmt"
+	"math/rand"
+
+	"github.com/pingcap/errors"
 	"github.com/qw4990/OptimizerTester/tidb"
 )
 
@@ -18,10 +22,94 @@ type datasetZipFX struct {
 func newDatasetZipFX(opt DatasetOpt, ins tidb.Instance) (Dataset, error) {
 	tbs := []string{"tint", "tdouble", "tstring", "tdatetime"}
 	cols := [][]string{{"a", "b"}, {"a", "b"}}
-	base, err := newBaseDataset(opt, ins, tbs, cols)
+	used := [][]bool{{true, true}, {true, true}}
+	base, err := newBaseDataset(opt, ins, tbs, cols, used)
 	return &datasetZipFX{base}, err
 }
 
 func (ds *datasetZipFX) Name() string {
 	return "ZipFX"
+}
+
+func (ds *datasetZipFX) GenCases(n int, qt QueryType) ([]string, error) {
+	if ds.orderedVals == nil {
+		if err := ds.init(); err != nil {
+			return nil, err
+		}
+	}
+
+	sqls := make([]string, 0, n)
+	switch qt {
+	case QTSingleColPointQuery:
+		for i := 0; i < n; i++ {
+			sqls = append(sqls, ds.randPointQuery(1))
+		}
+	case QTMultiColsPointQuery:
+		for i := 0; i < n; i++ {
+			sqls = append(sqls, ds.randPointQuery(2))
+		}
+	case QTSingleColRangeQuery:
+		for i := 0; i < n; i++ {
+			sqls = append(sqls, ds.randRangeQuery(1))
+		}
+	case QTMultiColsRangeQuery:
+		for i := 0; i < n; i++ {
+			sqls = append(sqls, ds.randRangeQuery(2))
+		}
+	case QTMultiColsRangeQueryEQPrefix:
+		for i := 0; i < n; i++ {
+			sqls = append(sqls, ds.randRangeQueryEQPrefix())
+		}
+	case QTMCVPointQuery:
+		for i := 0; i < n; i++ {
+			sqls = append(sqls, ds.randMCVLCVPointQuery(true))
+		}
+	case QTLCVPointQuery:
+		for i := 0; i < n; i++ {
+			sqls = append(sqls, ds.randMCVLCVPointQuery(false))
+		}
+	default:
+		return nil, errors.Errorf("unsupported query-type=%v", qt.String())
+	}
+	return sqls, nil
+}
+
+func (ds *datasetZipFX) randMCVLCVPointQuery(isMCV bool) string {
+	tbIdx := rand.Intn(4)
+	colIdx := rand.Intn(2)
+	val := ""
+	if isMCV {
+		val = ds.mcv[tbIdx][colIdx][rand.Intn(len(ds.mcv[tbIdx][colIdx]))]
+	} else {
+		val = ds.lcv[tbIdx][colIdx][rand.Intn(len(ds.lcv[tbIdx][colIdx]))]
+	}
+	return fmt.Sprintf("SELECT * FROM %v WHERE %v=%v", ds.tbs[tbIdx], ds.cols[colIdx], val)
+}
+
+func (ds *datasetZipFX) randRangeQueryEQPrefix() string {
+	tbIdx := rand.Intn(4)
+	cond := fmt.Sprintf("%v AND %v", ds.randPointColCond(tbIdx, 0), ds.randRangeColCond(tbIdx, 1))
+	return fmt.Sprintf("SELECT * FROM %v WHERE %v", ds.tbs[tbIdx], cond)
+}
+
+func (ds *datasetZipFX) randRangeQuery(cols int) string {
+	tbIdx := rand.Intn(4)
+	cond := ""
+	if cols == 1 {
+		cond = ds.randRangeColCond(tbIdx, rand.Intn(2))
+	} else {
+		cond = fmt.Sprintf("%v AND %v", ds.randRangeColCond(tbIdx, 0), ds.randRangeColCond(tbIdx, 1))
+	}
+	return fmt.Sprintf("SELECT * FROM %v WHERE %v", ds.tbs[tbIdx], cond)
+}
+
+func (ds *datasetZipFX) randPointQuery(cols int) string {
+	tbIdx := rand.Intn(4)
+	cond := ""
+	if cols == 1 {
+		cond = ds.randPointColCond(tbIdx, rand.Intn(2))
+	} else {
+		cond = fmt.Sprintf("%v AND %v", ds.randPointColCond(tbIdx, 0), ds.randPointColCond(tbIdx, 1))
+	}
+	return fmt.Sprintf("SELECT * FROM %v WHERE %v", ds.tbs[tbIdx], cond)
 }
