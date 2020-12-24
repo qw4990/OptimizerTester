2b595e55aaab97efd28c109d2ed80e829b83252b
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-12-24 19:36:21 +0800

    make IMDB support mul-cols tests

--- cetest/dataset_imdb.go
+++ cetest/dataset_imdb.go
@@ -22,5 +22,14 @@ func newDatasetIMDB(opt DatasetOpt) Dataset {
 				QTSingleColMCVPointOnCol:     {0, 0}, // SELECT * FROM title WHERE phonetic_code=?
 				QTSingleColMCVPointOnIndex:   {1, 0}, // SELECT * FROM cast_info WHERE person_id=?
 			}),
+		mciq: newMulColIndexQuerier(opt.DB,
+			[]string{"TITLE_production_year_episode_of_id_IDX"},
+			[]string{"title"},
+			[][]string{{"production_year", "episode_of_id"}},
+			[][]DATATYPE{{DTInt, DTInt}},
+			map[QueryType]int{
+				QTMulColsRangeQueryOnIndex: 0,
+				QTMulColsPointQueryOnIndex: 0,
+			}),
 	}}
 }
