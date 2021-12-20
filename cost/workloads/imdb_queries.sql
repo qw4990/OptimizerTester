5309b934dc701a086878689c321b6e6f4a203a42
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-12-20 18:05:03 +0800

    fixup

---
+++ cost/workloads/imdb_queries.sql
@@ -0,0 +1,87 @@
+CREATE TABLE `cast_info` (
+  `id` int(11) NOT NULL,
+  `person_id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL,
+  `person_role_id` int(11) DEFAULT NULL,
+  `note` text DEFAULT NULL,
+  `nr_order` int(11) DEFAULT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`) /*T![clustered_index] CLUSTERED */,
+  KEY `movie_id_cast_info` (`movie_id`),
+  KEY `person_id_cast_info` (`person_id`),
+  KEY `person_role_id_cast_info` (`person_role_id`),
+  KEY `role_id_cast_info` (`role_id`)
+);
+
+SELECT * FROM cast_info WHERE movie_id = ?;                         -- find the cast info of a movie
+SELECT * FROM cast_info WHERE person_id = ?;                        -- find the cast info of a person
+SELECT * FROM cast_info WHERE person_id = ? AND movie_id = ?;       -- find the cast info of a person in a movie
+SELECT * FROM cast_info WHERE person_id = ? AND person_role_id = ?; -- find the cast info of a person with a role
+
+
+CREATE TABLE `company_name` (
+  `id` int(11) NOT NULL,
+  `name` varchar(512) NOT NULL,
+  `country_code` varchar(6) DEFAULT NULL,
+  `imdb_id` int(11) DEFAULT NULL,
+  `name_pcode_nf` varchar(5) DEFAULT NULL,
+  `name_pcode_sf` varchar(5) DEFAULT NULL,
+  `md5sum` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`) /*T![clustered_index] CLUSTERED */
+);
+
+SELECT * FROM company_name WHERE id = ?;                            -- find a company
+SELECT * FROM company_name WHERE name = ?;                          -- find a company by the name
+SELECT * FROM company_name WHERE country_code = ?;                  -- find all companies with a code
+
+
+
+CREATE TABLE `movie_companies` (
+  `id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  `company_type_id` int(11) NOT NULL,
+  `note` text DEFAULT NULL,
+  PRIMARY KEY (`id`) /*T![clustered_index] CLUSTERED */,
+  KEY `company_id_movie_companies` (`company_id`),
+  KEY `company_type_id_movie_companies` (`company_type_id`),
+  KEY `movie_id_movie_companies` (`movie_id`)
+);
+
+SELECT * FROM movie_companies WHERE movie_id = ?;                   -- find the company of a movie
+SELECT * FROM movie_companies WHERE company_id = ?;                 -- find movies of a company
+
+CREATE TABLE `movie_keyword` (
+  `id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL,
+  `keyword_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`) /*T![clustered_index] CLUSTERED */,
+  KEY `keyword_id_movie_keyword` (`keyword_id`),
+  KEY `movie_id_movie_keyword` (`movie_id`)
+);
+
+SELECT * FROM movie_keyword WHERE movie_id = ?;                     -- find all keywords of a movie
+SELECT * FROM movie_keyword WHERE keyword_id = ?;                   -- find all movies with the keyword
+
+
+CREATE TABLE `title` (
+  `id` int(11) NOT NULL,
+  `title` varchar(512) NOT NULL,
+  `imdb_index` varchar(5) DEFAULT NULL,
+  `kind_id` int(11) NOT NULL,
+  `production_year` int(11) DEFAULT NULL,
+  `imdb_id` int(11) DEFAULT NULL,
+  `phonetic_code` varchar(5) DEFAULT NULL,
+  `episode_of_id` int(11) DEFAULT NULL,
+  `season_nr` int(11) DEFAULT NULL,
+  `episode_nr` int(11) DEFAULT NULL,
+  `series_years` varchar(49) DEFAULT NULL,
+  `md5sum` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`) /*T![clustered_index] CLUSTERED */,
+  KEY `kind_id_title` (`kind_id`)
+);
+
+SELECT * FROM title WHERE id = ?;                                   -- find the title of a movie
+SELECT * FROM title WHERE title = ?;                                -- find a movie by its title
+SELECT * FROM title WHERE production_year = ?;                      -- find movies by year
+SELECT * FROM title WHERE production_year BETWEEN ? AND ?;          -- find movies by year
---
+++ cost/workloads/tpch_queries.sql
@@ -0,0 +1,96 @@
+CREATE TABLE `customer` (
+  `C_CUSTKEY` bigint(20) NOT NULL,
+  `C_NAME` varchar(25) NOT NULL,
+  `C_ADDRESS` varchar(40) NOT NULL,
+  `C_NATIONKEY` bigint(20) NOT NULL,
+  `C_PHONE` char(15) NOT NULL,
+  `C_ACCTBAL` decimal(15,2) NOT NULL,
+  `C_MKTSEGMENT` char(10) NOT NULL,
+  `C_COMMENT` varchar(117) NOT NULL,
+  PRIMARY KEY (`C_CUSTKEY`) /*T![clustered_index] CLUSTERED */,
+  CONSTRAINT `CUSTOMER_FK1` FOREIGN KEY (`C_NATIONKEY`) REFERENCES `nation` (`N_NATIONKEY`)
+);
+
+SELECT * FROM customer WHERE C_CUSTKEY = ?;                             -- find a customer by its key
+SELECT * FROM customer WHERE C_NAME = ?;                                -- find customers by the name
+SELECT * FROM customer WHERE C_ADDRESS = ?;                             -- find customers by the address
+SELECT * FROM customer WHERE C_PHONE = ?;                               -- find customers by the phone
+
+
+CREATE TABLE `nation` (
+  `N_NATIONKEY` bigint(20) NOT NULL,
+  `N_NAME` char(25) NOT NULL,
+  `N_REGIONKEY` bigint(20) NOT NULL,
+  `N_COMMENT` varchar(152) DEFAULT NULL,
+  PRIMARY KEY (`N_NATIONKEY`) /*T![clustered_index] CLUSTERED */
+);
+
+SELECT * FROM nation WHERE N_NAME = ?;                                      -- find a nation by its name
+SELECT * FROM nation WHERE N_REGIONKEY = ?;                                 -- find nations by the region-key
+
+
+CREATE TABLE `supplier` (
+  `S_SUPPKEY` bigint(20) NOT NULL,
+  `S_NAME` char(25) NOT NULL,
+  `S_ADDRESS` varchar(40) NOT NULL,
+  `S_NATIONKEY` bigint(20) NOT NULL,
+  `S_PHONE` char(15) NOT NULL,
+  `S_ACCTBAL` decimal(15,2) NOT NULL,
+  `S_COMMENT` varchar(101) NOT NULL,
+  PRIMARY KEY (`S_SUPPKEY`) /*T![clustered_index] CLUSTERED */,
+  CONSTRAINT `SUPPLIER_FK1` FOREIGN KEY (`S_NATIONKEY`) REFERENCES `nation` (`N_NATIONKEY`)
+);
+
+SELECT * FROM supplier WHERE S_SUPPKEY = ?;                                 -- find a supplier by its key
+SELECT * FROM supplier WHERE S_NAME = ?;                                    -- find a supplier by its address
+
+
+CREATE TABLE `orders` (
+  `O_ORDERKEY` bigint(20) NOT NULL,
+  `O_CUSTKEY` bigint(20) NOT NULL,
+  `O_ORDERSTATUS` char(1) NOT NULL,
+  `O_TOTALPRICE` decimal(15,2) NOT NULL,
+  `O_ORDERDATE` date NOT NULL,
+  `O_ORDERPRIORITY` char(15) NOT NULL,
+  `O_CLERK` char(15) NOT NULL,
+  `O_SHIPPRIORITY` bigint(20) NOT NULL,
+  `O_COMMENT` varchar(79) NOT NULL,
+  PRIMARY KEY (`O_ORDERKEY`) /*T![clustered_index] CLUSTERED */,
+  CONSTRAINT `ORDERS_FK1` FOREIGN KEY (`O_CUSTKEY`) REFERENCES `customer` (`C_CUSTKEY`)
+);
+
+SELECT * FROM orders WHERE O_ORDERKEY = ?;                                  -- find a order by its key
+SELECT * FROM orders WHERE O_CUSTKEY = ?;                                   -- find orders by customer key
+SELECT * FROM orders WHERE O_ORDERDATE = ?;                                 -- find orders by date
+SELECT * FROM orders WHERE O_ORDERDATE BETWEEN ? AND ?;                     -- find orders by date
+
+
+CREATE TABLE `lineitem` (
+  `L_ORDERKEY` bigint(20) NOT NULL,
+  `L_PARTKEY` bigint(20) NOT NULL,
+  `L_SUPPKEY` bigint(20) NOT NULL,
+  `L_LINENUMBER` bigint(20) NOT NULL,
+  `L_QUANTITY` decimal(15,2) NOT NULL,
+  `L_EXTENDEDPRICE` decimal(15,2) NOT NULL,
+  `L_DISCOUNT` decimal(15,2) NOT NULL,
+  `L_TAX` decimal(15,2) NOT NULL,
+  `L_RETURNFLAG` char(1) NOT NULL,
+  `L_LINESTATUS` char(1) NOT NULL,
+  `L_SHIPDATE` date NOT NULL,
+  `L_COMMITDATE` date NOT NULL,
+  `L_RECEIPTDATE` date NOT NULL,
+  `L_SHIPINSTRUCT` char(25) NOT NULL,
+  `L_SHIPMODE` char(10) NOT NULL,
+  `L_COMMENT` varchar(44) NOT NULL,
+  PRIMARY KEY (`L_ORDERKEY`,`L_LINENUMBER`) /*T![clustered_index] NONCLUSTERED */,
+  CONSTRAINT `LINEITEM_FK1` FOREIGN KEY (`L_ORDERKEY`) REFERENCES `orders` (`O_ORDERKEY`),
+  CONSTRAINT `LINEITEM_FK2` FOREIGN KEY (`L_PARTKEY`,`L_SUPPKEY`) REFERENCES `partsupp` (`PS_PARTKEY`,`PS_SUPPKEY`)
+);
+
+SELECT * FROM lineitem WHERE L_QUANTITY BETWEEN ? AND ?;                        -- find lineitems by quantity
+SELECT * FROM lineitem WHERE L_EXTENDEDPRICE BETWEEN ? AND ?;                   -- find lineitems by price
+SELECT * FROM lineitem WHERE L_DISCOUNT BETWEEN ? AND ?;                        -- find lineitems by discount
+SELECT * FROM lineitem WHERE L_TAX BETWEEN ? AND ?;                             -- find lineitems by tax
+SELECT * FROM lineitem WHERE L_COMMITDATE BETWEEN ? AND ?;                      -- find lineitems by commit date
+SELECT * FROM lineitem WHERE L_RECEIPTDATE BETWEEN ? AND ?;                     -- find lineitems by receip date
+
