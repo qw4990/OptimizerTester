5827d5596146516c245d8fa252dc83af8b75fbb4
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-12-29 17:36:07 +0800

    fixup

--- cost/costcali.go
+++ cost/costcali.go
@@ -1,11 +1,5 @@
 package cost
 
-import (
-	"fmt"
-
-	"github.com/qw4990/OptimizerTester/tidb"
-)
-
 type FactorVector [6]float64 // (CPU, CopCPU, Net, Scan, DescScan, Mem)
 
 type FactorWeightsVector [6]float64 // (CPU, CopCPU, Net, Scan, DescScan, Mem)
@@ -19,34 +13,39 @@ type CaliQuery struct {
 type CaliQueries []CaliQuery
 
 type CaliRecord struct {
-	CaliQuery
-	TimeMS float64
+	Weights FactorWeightsVector
+	TimeMS  float64
 }
 
 type CaliRecords []CaliRecord
 
 // CostCalibration ...
 func CostCalibration() {
-	opt := tidb.Option{
-		Addr:     "172.16.5.173",
-		Port:     4000,
-		User:     "root",
-		Password: "",
-		Label:    "",
-	}
-	opt.Addr = "127.0.0.1"
+	//opt := tidb.Option{
+	//	Addr:     "172.16.5.173",
+	//	Port:     4000,
+	//	User:     "root",
+	//	Password: "",
+	//	Label:    "",
+	//}
+	//opt.Addr = "127.0.0.1"
+	//
+	//ins, err := tidb.ConnectTo(opt)
+	//if err != nil {
+	//	panic(err)
+	//}
 
-	ins, err := tidb.ConnectTo(opt)
-	if err != nil {
-		panic(err)
-	}
-
-	qs := genSyntheticCalibrationQueries(ins, "synthetic")
-	for _, q := range qs {
-		fmt.Println(q.SQL, q.Weights)
-	}
+	//qs := genSyntheticCalibrationQueries(ins, "synthetic")
+	//for _, q := range qs {
+	//	fmt.Println(q.SQL, q.Weights)
+	//}
 
 	// TODO: run qs and get rs
 
 	// TODO: do regression over rs
+
+	regressionCostFactors(CaliRecords{
+		{FactorWeightsVector{0, 0, 1, 1, 0, 0}, 1},
+		{FactorWeightsVector{0, 0, 1, 2, 0, 0}, 2},
+	})
 }
--- go.mod
+++ go.mod
@@ -4,18 +4,13 @@ go 1.13
 
 require (
 	github.com/BurntSushi/toml v0.3.1
-	github.com/cheggaaa/pb v1.0.29 // indirect
-	github.com/go-gota/gota v0.12.0 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/jung-kurt/gofpdf v1.16.2 // indirect
-	github.com/mattn/go-runewidth v0.0.12 // indirect
 	github.com/pingcap/errors v0.11.4
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/cobra v1.1.1
 	go.uber.org/atomic v1.4.0
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
 	gonum.org/v1/gonum v0.9.3 // indirect
 	gonum.org/v1/plot v0.9.0
-	gorgonia.org/gorgonia v0.9.17 // indirect
-	gorgonia.org/tensor v0.9.17 // indirect
+	gorgonia.org/gorgonia v0.9.17
+	gorgonia.org/tensor v0.9.17
 )
