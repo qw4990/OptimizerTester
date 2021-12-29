5d434b362b243d49ec8034322fd360beec9bd539
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-12-29 16:52:31 +0800

    fixup

--- cost/cali_synthetic.go
+++ cost/cali_synthetic.go
@@ -24,15 +24,27 @@ func genSyntheticCalibrationQueries(ins tidb.Instance, db string) CaliQueries {
 	n := 1
 	var ret CaliQueries
 	ret = append(ret, genSyntheticCaliScanQueries(ins, n)...)
+	ret = append(ret, genSyntheticCaliWideScanQueries(ins, n)...)
 	return ret
 }
 
 var hackRowSize map[string]float64
 
 func init() {
-	hackRowSize["Scan-TableScan-scan"] = 0
-	hackRowSize["Scan-TableScan-scan"] = 0
+	hackRowSize = make(map[string]float64)
+	hackRowSize["Scan-TableScan-scan"] = 20
+	hackRowSize["Scan-TableScan-net"] = 8.125
+	hackRowSize["Scan-IndexScan-scan"] = 29
+	hackRowSize["Scan-IndexScan-net"] = 8.125
+	hackRowSize["Scan-IndexLookup-scan"] = 38 + 20
+	hackRowSize["Scan-IndexLookup-net"] = 16.25 + 16.25
 
+	hackRowSize["WideScan-TableScan-scan"] = 20
+	hackRowSize["WideScan-TableScan-net"] = 139.23
+	hackRowSize["WideScan-IndexScan-scan"] = 160
+	hackRowSize["WideScan-IndexScan-net"] = 139.23
+	hackRowSize["WideScan-IndexLookup-scan"] = 38 + 20
+	hackRowSize["WideScan-IndexLookup-net"] = 16.25 + 139.23
 }
 
 func getSyntheticRowSize(key string) float64 {
@@ -41,6 +53,55 @@ func getSyntheticRowSize(key string) float64 {
 	}
 	return hackRowSize[key]
 }
+
+
+func genSyntheticCaliWideScanQueries(ins tidb.Instance, n int) CaliQueries {
+	var qs CaliQueries
+	var minA, maxA, minB, maxB int
+	mustReadOneLine(ins, `select min(a), max(a), min(b), max(b) from t`, &minA, &maxA, &minB, &maxB)
+
+	// PK scan
+	for i := 0; i < n; i++ {
+		l, r := randRange(minA, maxA, i, n)
+		rowCount := mustGetRowCount(ins, fmt.Sprintf("select count(*) from t where a>=%v and a<=%v", l, r))
+		scanW := float64(rowCount) * getSyntheticRowSize("WideScan-TableScan-scan")
+		netW := float64(rowCount) * getSyntheticRowSize("WideScan-TableScan-net")
+		qs = append(qs, CaliQuery{
+			SQL:          fmt.Sprintf("select /*+ use_index(t, primary) */ a, c from t where a>=%v and a<=%v", l, r),
+			Label:        "",
+			FactorVector: [6]float64{0, 0, netW, scanW, 0, 0},
+		})
+	}
+
+	// index scan
+	for i := 0; i < n; i++ {
+		l, r := randRange(minB, maxB, i, n)
+		rowCount := mustGetRowCount(ins, fmt.Sprintf("select count(*) from t where b>=%v and b<=%v", l, r))
+		scanW := float64(rowCount) * getSyntheticRowSize("WideScan-IndexScan-scan")
+		netW := float64(rowCount) * getSyntheticRowSize("WideScan-IndexScan-net")
+		qs = append(qs, CaliQuery{
+			SQL:          fmt.Sprintf("select /*+ use_index(t, bc) */ b, c from t where b>=%v and b<=%v", l, r),
+			Label:        "",
+			FactorVector: [6]float64{0, 0, netW, scanW, 0, 0},
+		})
+	}
+
+	// index lookup
+	for i := 0; i < n; i++ {
+		l, r := randRange(minB, maxB, i, n)
+		rowCount := mustGetRowCount(ins, fmt.Sprintf("select count(*) from t where b>=%v and b<=%v", l, r))
+		scanW := float64(rowCount) * getSyntheticRowSize("WideScan-IndexLookup-scan")
+		netW := float64(rowCount) * getSyntheticRowSize("WideScan-IndexLookup-net")
+		qs = append(qs, CaliQuery{
+			SQL:          fmt.Sprintf("select /*+ use_index(t, b) */ b, c from t where b>=%v and b<=%v", l, r),
+			Label:        "",
+			FactorVector: [6]float64{0, 0, netW, scanW, 0, 0},
+		})
+	}
+
+	return qs
+}
+
 
 func genSyntheticCaliScanQueries(ins tidb.Instance, n int) CaliQueries {
 	var qs CaliQueries
