499f7f4ae72999a769888ca1fba026901f478af7
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-12-29 17:32:29 +0800

    fixup

--- cost/costcali_regression.go
+++ cost/costcali_regression.go
@@ -1,12 +1,99 @@
 package cost
 
 import (
+	"fmt"
+	"math"
+	
+	"gorgonia.org/gorgonia"
+	"gorgonia.org/tensor"
 )
 
 func regressionCostFactors(rs CaliRecords) FactorVector {
-
-
+	x, y := convert2XY(rs)
 	g := gorgonia.NewGraph()
-	
-	return nil
+	xNode := gorgonia.NodeFromAny(g, x, gorgonia.WithName("x"))
+	yNode := gorgonia.NodeFromAny(g, y, gorgonia.WithName("y"))
+	costFactor := gorgonia.NewVector(g, gorgonia.Float64,
+		gorgonia.WithName("cost-factor"),
+		gorgonia.WithShape(xNode.Shape()[1]),
+		gorgonia.WithInit(gorgonia.Uniform(0, 20)))
+
+	pred := must(gorgonia.Mul(xNode, costFactor))
+	var predicated gorgonia.Value
+	gorgonia.Read(pred, &predicated)
+
+	squaredError := must(gorgonia.Square(must(gorgonia.Sub(pred, yNode))))
+	loss := must(gorgonia.Mean(squaredError))
+	if _, err := gorgonia.Grad(loss, costFactor); err != nil {
+		panic(fmt.Sprintf("Failed to backpropagate: %v", err))
+	}
+
+	solver := gorgonia.NewVanillaSolver(gorgonia.WithLearnRate(0.001))
+	model := []gorgonia.ValueGrad{costFactor}
+
+	machine := gorgonia.NewTapeMachine(g, gorgonia.BindDualValues(costFactor))
+	defer machine.Close()
+
+	iter := 1000
+	for i := 0; i < iter; i++ {
+		if err := machine.RunAll(); err != nil {
+			panic(fmt.Sprintf("Error during iteration: %v: %v\n", i, err))
+		}
+
+		if err := solver.Step(model); err != nil {
+			panic(err)
+		}
+
+		machine.Reset()
+		fmt.Printf("theta: %2.2f  Iter: %v Cost: %2.3f Accuracy: %2.2f \r",
+			costFactor.Value(),
+			i,
+			loss.Value(),
+			accuracy(predicated.Data().([]float64), yNode.Value().Data().([]float64)))
+	}
+
+	return FactorVector{}
+}
+
+func convert2XY(rs CaliRecords) (*tensor.Dense, *tensor.Dense) {
+	by := make([]float64, 0, len(rs))
+	for _, r := range rs {
+		by = append(by, r.TimeMS)
+	}
+	y := tensor.New(tensor.WithShape(len(rs)), tensor.WithBacking(by))
+
+	bx := make([]float64, 0, len(rs)*len(rs[0].Weights))
+	for _, r := range rs {
+		for _, w := range r.Weights {
+			bx = append(bx, w)
+		}
+	}
+	x := tensor.New(tensor.WithShape(len(rs), len(rs[0].Weights)), tensor.WithBacking(bx))
+
+	return x, y
+}
+
+func must(n *gorgonia.Node, err error) *gorgonia.Node {
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func one(size int) []float64 {
+	one := make([]float64, size)
+	for i := 0; i < size; i++ {
+		one[i] = 1.0
+	}
+	return one
+}
+
+func accuracy(prediction, y []float64) float64 {
+	var ok float64
+	for i := 0; i < len(prediction); i++ {
+		if math.Round(prediction[i]-y[i]) == 0 {
+			ok += 1.0
+		}
+	}
+	return ok / float64(len(y))
 }
--- go.mod
+++ go.mod
@@ -4,17 +4,18 @@ go 1.13
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/cheggaaa/pb v1.0.29 // indirect
+	github.com/go-gota/gota v0.12.0 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jung-kurt/gofpdf v1.16.2 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
 	github.com/pingcap/errors v0.11.4
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/cobra v1.1.1
 	go.uber.org/atomic v1.4.0
-	gonum.org/v1/plot v0.8.1
-	github.com/cheggaaa/pb v1.0.29
-	github.com/go-gota/gota v0.12.0
-	github.com/mattn/go-runewidth v0.0.12 // indirect
-	github.com/pkg/errors v0.9.1
-	github.com/rivo/uniseg v0.2.0 // indirect
-	gonum.org/v1/gonum v0.9.3
-	gorgonia.org/gorgonia v0.9.17
-	gorgonia.org/tensor v0.9.17
+	gonum.org/v1/gonum v0.9.3 // indirect
+	gonum.org/v1/plot v0.9.0
+	gorgonia.org/gorgonia v0.9.17 // indirect
+	gorgonia.org/tensor v0.9.17 // indirect
 )
