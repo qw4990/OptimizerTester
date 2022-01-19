f08fb6499968be594729850d3f8b109d835caefc
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2022-01-19 14:43:08 +0800

    fixup

--- cost/costcali.go
+++ cost/costcali.go
@@ -99,7 +99,7 @@ func CostCalibration() {
 			fmt.Printf("[cost-eval] run %v/%v timeMS:%v, SQL:%v, costAct:%v, costCal: %v\n", i, len(qs), timeMS, qs[i].SQL, planCost, calCost)
 
 			costDelta := math.Abs(planCost - calCost)
-			if costDelta > 50 && costDelta/planCost > 0.1 {
+			if costDelta > 50 && costDelta/planCost > 0.05 {
 				panic("wrong cal-cost")
 			}
 		}
