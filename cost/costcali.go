6f082d0ff7bd53f7a4fa0ceed6835f4bc1a9f754
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2022-02-15 14:54:13 +0800

    fixup

--- cost/costcali.go
+++ cost/costcali.go
@@ -3,6 +3,7 @@ package cost
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 const NumFactors = 7
@@ -41,117 +42,52 @@ func NewCostWeights(cpu, copCPU, net, scan, descScan, mem, seek float64) CostWei
 	return CostWeights{cpu, copCPU, net, scan, descScan, mem, seek}
 }
 
-type CaliQuery struct {
+type CaliRecord struct {
 	SQL     string
 	Label   string
 	Weights CostWeights
-}
-
-type CaliQueries []CaliQuery
-
-type CaliRecord struct {
-	CaliQuery
-	TimeNS float64
-	Cost   float64
+	TimeNS  float64
+	Cost    float64
 }
 
 type CaliRecords []CaliRecord
 
 // CostCalibration ...
 func CostCalibration() {
-	//opt := tidb.Option{
-	//	Addr:     "172.16.5.173",
-	//	Port:     4000,
-	//	User:     "root",
-	//	Password: "",
-	//	Label:    "",
-	//}
-	//
-	//ins, err := tidb.ConnectTo(opt)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//db := "synthetic"
-	//queryFile := filepath.Join("/tmp/cost-calibration", fmt.Sprintf("%v-caliqueries.json", db))
-	//recordFile := filepath.Join("/tmp/cost-calibration", fmt.Sprintf("%v-calirecords.json", db))
-	//
-	//var qs CaliQueries
-	//if err := readFrom(queryFile, &qs); err != nil {
-	//	fmt.Println("[cost-eval] read cali-queries file error: ", err)
-	//	qs = genSyntheticCalibrationQueries(ins, db)
-	//	fmt.Printf("[cost-eval] gen %v cali-queries for %v\n", len(qs), db)
-	//	saveTo(queryFile, qs)
-	//} else {
-	//	fmt.Println("[cost-eval] read cali-queries from file successfully ")
-	//}
-	//
-	//var rs CaliRecords
-	//costFactors := readCostFactors(ins)
-	//if err := readFrom(recordFile, &rs); err != nil {
-	//	fmt.Println("[cost-eval] read cali-records file error: ", err)
-	//
-	//	ins.MustExec(fmt.Sprintf(`use %v`, db))
-	//	ins.MustExec(`set @@tidb_index_lookup_size=1024`)
-	//	ins.MustExec(`set @@tidb_cost_calibration_mode=2`)
-	//	ins.MustExec(`set @@tidb_distsql_scan_concurrency=1`)
-	//	ins.MustExec(`set @@tidb_executor_concurrency=1`)
-	//	ins.MustExec(`set @@tidb_opt_tiflash_concurrency_factor=1`)
-	//	ins.MustExec(`set @@tidb_cost_variant=1`)
-	//	rs = make(CaliRecords, 0, len(qs))
-	//	for i := range qs {
-	//		// check cost weights
-	//		calCost := calculateCost(qs[i].Weights, costFactors)
-	//
-	//		_, planCost, timeMS, _ := extractCostTimeFromQuery(ins, qs[i].SQL, 10, 0, false, nil)
-	//		rs = append(rs, CaliRecord{
-	//			CaliQuery: qs[i],
-	//			Cost:      planCost,
-	//			TimeNS:    timeMS * float64(time.Millisecond/time.Nanosecond),
-	//		})
-	//		fmt.Printf("[cost-eval] run %v/%v timeMS:%v, SQL:%v, costAct:%v, costCal: %v\n", i, len(qs), timeMS, qs[i].SQL, planCost, calCost)
-	//
-	//		costDelta := math.Abs(planCost - calCost)
-	//		if costDelta > 50 && costDelta/planCost > 0.05 {
-	//			panic("wrong cal-cost")
-	//		}
-	//	}
-	//	fmt.Printf("[cost-eval] gen %v cali-records for %v\n", len(qs), db)
-	//	saveTo(recordFile, rs)
-	//} else {
-	//	fmt.Println("[cost-eval] read cali-records from file successfully")
-	//}
-	//
-	///*
-	//	Table/IndexScan + Wide-Table/IndexScan ==> Scan = 25 * Net
-	//	IndexScan + DescIndexScan ==> DescScan = 1.5 * Scan
-	//	TableScan + DescTableScan ==> DescScan = 1.8 * Scan
-	//	Table/IndexScan/Lookup + Wide-* ==> Seek = 1750000 * Net
-	//	IndexScan + Wide-IndexScan + Agg-NotPushedDown + Sort ==> CPU = 7 * Net
-	//
-	//	(CPU,	CopCPU,	Net,	Scan,	DescScan,	Mem,	Seek)
-	//	(30,	30,		4,		100,	150,		0,		1750000)
-	//*/
-	//whilteList := []string{
-	//	"TableScan",
-	//	"IndexScan",
-	//	//"IndexLookup",
-	//	"Wide-TableScan",
-	//	"Wide-IndexScan",
-	//	//"Wide-IndexLookup",
-	//	//"DescTableScan",
-	//	//"DescIndexScan",
-	//	//"Agg-PushedDown",
-	//	"Agg-NotPushedDown",
-	//	"Sort",
-	//}
-	//
-	//rs = filterCaliRecordsByLabel(rs, whilteList, nil)
-	//// (CPU, CopCPU, Net, Scan, DescScan, Mem, Seek)
-	//rs = maskRecords(rs, [NumFactors]bool{true, false, true, true, false, false, false})
-	//
-	//ret := regressionCostFactors(rs)
-	//fmt.Println(ret.String())
+	recordsPath := "./cost-calibration-data/cost-calibration-data/synthetic-original-records.json"
+	var rs Records
+	if err := readFrom(recordsPath, &rs); err != nil {
+		panic(err)
+	}
+	var crs CaliRecords
+	for _, r := range rs {
+		crs = append(crs, CaliRecord{
+			SQL:     r.SQL,
+			Label:   r.Label,
+			Weights: r.CostWeights,
+			TimeNS:  r.TimeMS * float64(time.Millisecond/time.Nanosecond),
+			Cost:    r.Cost,
+		})
+	}
+
+	whiteList := []string{
+		"TableScan",
+		"IndexScan",
+		"WideTableScan",
+		"WideIndexScan",
+		"DescTableScan",
+		"DescIndexScan",
+		//"IndexLookup",
+		//"Wide-IndexLookup",
+		//"Agg-PushedDown",
+		//"Agg-NotPushedDown",
+		//"Sort",
+	}
+	crs = filterCaliRecordsByLabel(crs, whiteList, nil)
+	// (CPU, CopCPU, Net, Scan, DescScan, Mem, Seek)
+	//crs = maskRecords(crs, [NumFactors]bool{true, false, true, true, false, false, false})
+	ret := regressionCostFactors(crs)
+	fmt.Println(ret.String())
 }
 
 func maskRecords(rs CaliRecords, mask [NumFactors]bool) CaliRecords {
